978cfd3208f68b1edc604e2a9edd4b393dfe8f19
순민 <tkksm2@gmail.com>
2022-05-21 00:45:07 +0900

    Feat: vite env 설정 완료

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -8,7 +8,7 @@
 import { onMounted } from 'vue';
 import { KAKAO_JS_KEY } from '@/utils/key';
 onMounted(() => {
-  console.log("import.meta.env", import.meta.env.VITE_TEST_VAR);
+  console.log("import.meta.env", import.meta.env.VITE_KAKAO_JS_KEY);
   console.log("KAKAO_JS_KEY", KAKAO_JS_KEY);
 })
 </script>
--- src/utils/key.ts
+++ src/utils/key.ts
@@ -1,2 +1,2 @@
 export const KAKAO_JS_KEY =
-  import.meta.env.VITE_TEST_VAR || 'fadc324ac529429af99cb371004d2ff5';
+  import.meta.env.VITE_KAKAO_JS_KEY || 'fadc324ac529429af99cb371004d2ff5';
