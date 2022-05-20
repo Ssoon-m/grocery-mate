9c6cb146d8894c861c5d22e7f21630a2ab9c5e9f
순민 <tkksm2@gmail.com>
2022-05-21 00:15:21 +0900

    Feat: env setting

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { KAKAO_JS_KEY } from '../../utils/key';
+onMounted(() => {
+  console.log("import.meta.env", import.meta.env.KAKAO_JS_KEY)
+  console.log("KAKAO_JS_KEY", KAKAO_JS_KEY);
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ utils/key.ts
@@ -0,0 +1,3 @@
+export const KAKAO_JS_KEY = import.meta.env.local
+  ? 'fadc324ac529429af99cb371004d2ff5'
+  : 'hello';
