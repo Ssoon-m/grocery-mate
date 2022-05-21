df213945bdb9c75d857882778ff41321389a0ea3
순민 <tkksm2@gmail.com>
2022-05-21 14:16:57 +0900

    Feat: sass 라이브러리 추가

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,13 @@ import TestComponent from './components/TestComponent.vue';
   </header>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
   font-weight: normal;
 }
-
 
 @media (hover: hover) {
   a:hover {
