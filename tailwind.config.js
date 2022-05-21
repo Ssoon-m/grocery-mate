30dc8e137219da6662e6a0bcdc7cc2b256981180
순민 <tkksm2@gmail.com>
2022-05-21 14:37:35 +0900

    tailwind 설정에 대한  설명 주석처리

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 // 스타일 커스텀 하는법
 // https://tailwindcss.com/docs/adding-custom-styles
+
+// https://tailwindcss.com/docs/dark-mode <- 다크모드 적용법
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+  },
+  fontFamily: {
+    // sans: ['Graphik', 'sans-serif'],
+    // serif: ['Merriweather', 'serif'],
   },
   plugins: [],
 };
